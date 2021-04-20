42e1989238e97195a893d5a12f894b0ba963e671
zaheersani <sani.zaheer@gmail.com>
2021-04-20 15:27:40 +0500

    On Screen Numeric Pad with Event Handler

--- App.js
+++ App.js
@@ -7,42 +7,57 @@ export default function App() {
   const [getText, setText] = useState("0")
 
   const buttonClick = (txt) => {
-    // alert(txt)
+    console.log(txt)
     setText(getText + txt)
   }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Header fullname="Zaheer Sani" />
-      <Text style={{ fontSize: 50 }}>{getText}</Text>
+      <Text style={{ fontSize: 30, paddingBottom: 100 }}>Guess a Number Game</Text>
+      <Text style={{ fontSize: 50, paddingBottom: 50 }}>{getText}</Text>
       <View style={{ flexDirection: 'row' }}>
-        <View style={{ width: 50 }}>
+        <View style={styles.buttonContainer}>
           <Button title="1" color="green" onPress={buttonClick.bind(this, 1)} />
         </View>
-        <Button title="2" color="red" onPress={buttonClick.bind(this, 2)} />
-        <Button title="3" color="yellow" onPress={buttonClick.bind(this, 3)} />
+        <View style={styles.buttonContainer}>
+          <Button title="2" color="red" onPress={buttonClick.bind(this, 2)} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="3" color="blue" onPress={buttonClick.bind(this, 3)} />
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.buttonContainer}>
+          <Button title="4" color="green" onPress={buttonClick.bind(this, 1)} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="5" color="red" onPress={buttonClick.bind(this, 2)} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="6" color="blue" onPress={buttonClick.bind(this, 3)} />
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.buttonContainer}>
+          <Button title="7" color="green" onPress={buttonClick.bind(this, 1)} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="8" color="red" onPress={buttonClick.bind(this, 2)} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="9" color="blue" onPress={buttonClick.bind(this, 3)} />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const Header = (props) => {
-  return (
-    <View>
-      <Text style={headerStyles.text}>Name: {props.fullname}</Text>
-    </View>
-  )
-}
-
-const headerStyles = StyleSheet.create({
-  text: {
-    fontSize: 20
-  }
-})
-
 const styles = StyleSheet.create({
+  buttonContainer: {
+    width: '30%',
+    padding: 10
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
