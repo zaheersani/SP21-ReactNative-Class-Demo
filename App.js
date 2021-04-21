228e20267c232d3f262dd2b3495854d0eb4ee5f5
zaheersani <sani.zaheer@gmail.com>
2021-04-21 12:25:15 +0500

    Discount is calculated | Header is Added | Next: Currency Format

--- App.js
+++ App.js
@@ -4,42 +4,62 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 export default function App() {
   const [getPrice, setPrice] = useState();
   const [getDiscount, setDiscount] = useState();
+  const [getSaveValue, setSaveValue] = useState(0);
+  const [getFinalPrice, setFinalPrice] = useState(0);
 
+  var save = 50
   useEffect(() => {
-    const price = getPrice
-    const discount = getDiscount
-    if (price != undefined && discount != undefined) {
-      save = discount / 100 * price;
-      finalPrice = price - save;
-      console.log(`You Save: ${save}, Final Price: ${finalPrice}`);
+    if (getPrice != undefined && getDiscount != undefined) {
+      console.log(`Price: ${getPrice}, Discount: ${getDiscount}`)
+      save = getDiscount / 100 * getPrice;
+      setSaveValue(save);
+      const discountedPrice = getPrice - save;
+      setFinalPrice(discountedPrice);
+      console.log(`You Save: ${save}, Final Price: ${discountedPrice}`)
     }
-  });
+  })
+
+  const isValidInput = (text) => {
+    return !isNaN(text) && text >= 0 && text <= 100
+  }
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Original Price"
-        keyboardType={'decimal-pad'}
-        onChangeText={(text) => setPrice(text)}
-        value={getPrice}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Discount %"
-        maxLength={3}
-        keyboardType={'decimal-pad'}
-        onChangeText={(text) => setDiscount(text)}
-        value={getDiscount}
-      />
-      <View style={styles.calculationContainer}>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>You Save: </Text>
-          <Text style={styles.text}>{0}</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>Final Price: </Text>
-          <Text style={styles.text}>{0}</Text>
+    <View>
+      <Header />
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Original Price"
+          maxLength={15}
+          keyboardType={'decimal-pad'}
+          onChangeText={(text) => {
+            if (!isNaN(text)) {
+              setPrice(text);
+            }
+          }}
+          value={getPrice}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Discount %"
+          maxLength={3}
+          keyboardType={'decimal-pad'}
+          onChangeText={(text) => {
+            if (isValidInput(text)) {
+              setDiscount(text)
+            }
+          }}
+          value={getDiscount}
+        />
+        <View style={styles.calculationContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>You Save: </Text>
+            <Text style={styles.text}>{getSaveValue}</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Final Price: </Text>
+            <Text style={styles.text}>{getFinalPrice}</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -48,24 +68,57 @@ export default function App() {
 
 const styles = StyleSheet.create({
   calculationContainer: {
-    paddingTop: 30, height: 100, width: "70%", justifyContent: 'space-between'
+    paddingTop: 30,
+    height: 130,
+    width: "80%",
+    justifyContent: 'space-between'
   },
-  textContainer: { flexDirection: "row", justifyContent: 'space-between' },
+  textContainer: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    backgroundColor: 'purple',
+    padding: 10,
+    elevation: 5
+  },
   input: {
-    borderColor: 'grey',
+    borderColor: 'purple',
     borderBottomWidth: 2,
     width: "80%",
     height: 50,
     fontSize: 20
   },
   text: {
-    fontSize: 20
+    fontSize: 20,
+    color: "white"
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'flex-start',
-    paddingTop: 50,
+    paddingTop: 30,
     paddingLeft: 50,
   },
+});
+
+const Header = () => {
+  return (
+    <View style={headerStyle.container}>
+      <Text style={headerStyle.title}>Discount Calculator</Text>
+    </View>
+  )
+}
+
+const headerStyle = StyleSheet.create({
+  container: {
+    backgroundColor: 'purple',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 90,
+    elevation: 5
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    paddingTop: 30,
+  }
 });
